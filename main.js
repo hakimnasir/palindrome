c6010fed7f9c5288ad30db0d5162a45a5cbe5658
nasir hakim <titans8pride@gmail.com>
2018-05-16 15:58:43 -0700

    Working palindrome site

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 
-let Phrase = require("nhakim-palindrome");
+let Phrase = require("mhartl-palindrome");
 
-alert(new Phrase("Madam, I'm Adam.").palindrome());
+let string = prompt("Please enter a string for palindrome testing");
+
+let phrase = new Phrase(string);
+
+if (phrase.palindrome()) {
+	alert(`"${phrase.content}" is a palindrome`);
+} else {
+	alert(`"${phrase.content}" is nto a plindrome`)
+}
